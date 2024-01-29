754387ace75fc505d08ba78757903af4f2f1fbd4
becki <berekeetabera@gmail.com>
2024-01-29 13:31:57 +0300

    Finalizing the Counter Bloc Practice

---
+++ lib/app.dart
@@ -0,0 +1,6 @@
+import 'package:bloc_counter/view/counter_page.dart';
+import 'package:flutter/material.dart';
+
+class CounterApp extends MaterialApp {
+  CounterApp({super.key}) : super(home: CounterPage());
+}
